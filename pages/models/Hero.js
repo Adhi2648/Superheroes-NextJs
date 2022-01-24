adea3ccd60a527d0f668ae39f2b0eab783f20ade
Adhi2648 <adhinaryanan619@gmail.com>
2022-01-24 22:46:49 +0530

    first commit

---
+++ pages/models/Hero.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const HeroSchema = new mongoose.Schema({
+  superHero: {
+    type: String,
+    required: [true, "Please name the hero"],
+    unique: true,
+    trim: true,
+  },
+  realName: {
+    type: String,
+    required: true,
+    maxlength: [200, "Please keep real name short"],
+  },
+});
